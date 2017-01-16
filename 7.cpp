38b0d612c35fa6f2c494d4b9659aedae19e40976
Aneesh <aneesh.relan@gmail.com>
2017-01-17 01:18:45 +0530

    Problem 7

---
+++ 7.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+	int n = 10001;
+	long long int size = n*log(n*log(n));
+
+	int *nums = new int[size];
+
+	memset(nums,0,sizeof(&nums));
+	int count =0;
+	for(int i = 2; i<size; i++)
+	{
+		if(nums[i] == 0)
+		{
+			
+			count++;
+
+			if(count == n)
+			{
+				printf("%d\n", i);
+				break;
+			}
+			for(int j = 2*i; j<size; j+=i)
+				nums[j] = 1;
+		}
+	}
+
+	return 0;
+}
