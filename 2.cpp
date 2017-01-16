eb9a336fcd3d26e29d44fea201ffd50475169788
Aneesh <aneesh.relan@gmail.com>
2017-01-17 00:34:31 +0530

    Program 2

---
+++ 2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long int a,b,c,sum;
+	a = 0;
+	b = 1;
+	c = 0;
+	sum = 0;
+	while(c < 4000000)
+	{
+		c = a+b;
+		b = a;
+		a = c;
+
+		if(c % 2 == 0)
+			sum += c;
+	}
+	printf("%lld\n",sum);
+
+	return 0;
+}
