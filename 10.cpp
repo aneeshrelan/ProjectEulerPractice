b385d52b1ffa35ba2a412d170ce908345d703dec
Aneesh <aneesh.relan@gmail.com>
2017-01-17 01:51:19 +0530

    Problem 10

---
+++ 10.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+	int nums[2000000];
+
+	memset(nums,0,sizeof(nums));
+	long long int i,j,sum=0;
+	for(i = 2; i<2000000; i++)
+	{
+		if(nums[i] == 0)
+		{
+			sum += i;
+			for(j = 2*i; j<2000000; j+=i)
+				nums[j] = 1;
+		}
+	}
+	printf("%lld\n", sum);
+
+	return 0;
+}
